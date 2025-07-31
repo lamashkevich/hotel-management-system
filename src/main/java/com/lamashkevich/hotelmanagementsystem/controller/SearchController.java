5ec3ffba695de1acd349952e70c3f3ce8a546061
Yauheni Lamashkevich <lamashkevich.evg@gmail.com>
2025-07-31 15:31:50 +0300

    Add swagger

--- src/main/java/com/lamashkevich/hotelmanagementsystem/controller/SearchController.java
+++ src/main/java/com/lamashkevich/hotelmanagementsystem/controller/SearchController.java
@@ -3,6 +3,10 @@ package com.lamashkevich.hotelmanagementsystem.controller;
 import com.lamashkevich.hotelmanagementsystem.dto.HotelFilter;
 import com.lamashkevich.hotelmanagementsystem.dto.HotelPreviewResponse;
 import com.lamashkevich.hotelmanagementsystem.service.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
+@Tag(name = "Search hotels")
 @RestController
 @RequestMapping("/property-view/search")
 @RequiredArgsConstructor
@@ -19,6 +24,14 @@ public class SearchController {
 
     private final SearchService searchService;
 
+    @Operation(
+            summary = "Search hotels by filter",
+            description = "Returns a list of all hotels matching the filter with their brief information."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Hotels found"),
+            @ApiResponse(responseCode = "400", description = "Invalid filter")
+    })
     @GetMapping
     public List<HotelPreviewResponse> search(@Valid HotelFilter hotelFilter) {
         return searchService.searchByFilter(hotelFilter);
